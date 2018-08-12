df499856f41f96a38f308a4776c89e1ca4212f0c
me.mojila <me.mojila@Mochs-MacBook.local>
2018-08-12 15:04:31 +0700

    add preact

--- next.config.js
+++ next.config.js
@@ -2,9 +2,11 @@ const withPlugins = require('next-compose-plugins');
 const css = require('@zeit/next-css');
 const imageOptimized = require('next-optimized-images');
 const purgeCSS = require('next-purgecss');
+const preact = require('@zeit/next-preact');
 
 module.exports = withPlugins([
     [css],
     [imageOptimized],
-    [purgeCSS]
+    [purgeCSS],
+    [preact]
 ]);
